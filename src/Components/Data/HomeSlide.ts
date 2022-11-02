f6ff37b01a947e24078d60141066db3da3933d94
nhokphal <phalnhok@gmail.com>
2022-11-02 16:35:47 +0700

    first commit

---
+++ src/Components/Data/HomeSlide.ts
@@ -0,0 +1,14 @@
+export interface HomeDTB  {
+        id: number;
+        img: string;
+        title: string;
+        desc: string;
+}
+
+
+export const SlideItem : HomeDTB [] = [{
+        id: 1,
+        img: "https://i.ibb.co/DG69bQ4/1.png",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+}]
